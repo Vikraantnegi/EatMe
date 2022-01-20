506783571a08cd8d1b4c08e269620dc3f958f137
vikrant <vikrant.negi74@gmail.com>
2022-01-20 14:59:41 +0530

    Home Boiler Plate

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { View, Text, TouchableOpacity, TouchableWithoutFeedback, Image, FlatList } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import { connect } from 'react-redux';
+import { setSelectedTab } from '../app/Sidebar/TabActions';
+import { Home, Search, CartTab, Favourite, Notification } from '../screens';
+import { COLORS, FONTS, SIZES, icons, constants, dummyData  } from '../constants';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
-const MainLayout = ({drawerAnimationStyle}) => {
+const MainLayout = (props) => {
+    const { drawerAnimationStyle, navigation, selectedTab, setSelectedTab } = {...props}
     return (
         <Animated.View
             style={{
@@ -18,4 +24,16 @@ const MainLayout = ({drawerAnimationStyle}) => {
     )
 }
 
-export default MainLayout;
+function mapStateToProps(state){
+    return {
+        selectedTab: state.tabs.selectedTab
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        setSelectedTab: (selectedTab) => dispatch(setSelectedTab(selectedTab))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
