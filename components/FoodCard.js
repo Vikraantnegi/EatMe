369e7fc7452753175f104d8ded489a5e1f585fee
vikrant <vikrant.negi74@gmail.com>
2022-01-22 19:44:27 +0530

    Food Card Component Complete

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -3,12 +3,46 @@ import React from 'react';
 import { COLORS, FONTS, SIZES, icons } from '../constants';
 
 const FoodCard = (props) => {
-  const { containerStyle, item, ImageStyle, onPress } = {...props}
+  const { containerStyle, item, imageStyle, onPress } = {...props}
   return (
     <TouchableOpacity
         style={{ flexDirection: 'row', borderRadius: SIZES.radius, backgroundColor: COLORS.lightGray2, ...containerStyle }}
     >
-        
+        <View style={{ flexDirection: 'row', position: 'absolute', top: 5, right: SIZES.radius }}>
+            <Image 
+                source={icons.calories}
+                style={{ width: 30, height: 30 }}
+            />
+            <Text 
+                style={{ color: COLORS.darkGray2, ...FONTS.body5 }} 
+            >
+                {item.calories} Calories
+            </Text>
+        </View>
+        <Image
+            style={{ ...imageStyle }}
+            source={item.image}
+        />
+        <View
+            style={{ flex: 1 }}
+        >
+            <Text
+                style={{ ...FONTS.h3, fontSize: 17, color: COLORS.black }}
+            >
+                {item.name}
+            </Text>
+            <Text
+                style={{ ...FONTS.body4, color: COLORS.darkGray2 }}
+            >
+                {item.description}
+            </Text>
+            <Text
+                style={{ ...FONTS.h2, marginTop: SIZES.base, color: COLORS.black }} 
+            >
+                ${item.price}
+            </Text>
+        </View>
+
     </TouchableOpacity>
   );
 };
