32864e0ae14d7a12c47c88a594a85b79a4a388c1
vikrant <vikrant.negi74@gmail.com>
2022-01-22 15:18:31 +0530

    Home Lists Setup

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, Image, TextInput, FlatList } from 'react-native';
 import { SearchComponent } from '../../components';
 import { FONTS, SIZES, icons, dummyData, COLORS } from '../../constants';
 
 const Home = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState(1)
+    const [selectedMenu, setSelectedMenu] = useState(1)
+    const [menuList, setMenuList] = useState([])
+
+    useEffect(() => {
+        handleChangeMenu( selectedCategory, selectedMenu );
+    })
+
+    const handleChangeMenu = (categoryId, menuId) => {
+        let selectedMenu = dummyData.menu.find(a => a.id === menuId)
+        setMenuList(selectedMenu?.list.filter( a => a.category.includes(categoryId)))
+    }
+
     return (
         <View
             style={{
@@ -11,7 +25,19 @@ const Home = () => {
             }}
         >
             <SearchComponent />
-               
+            
+            <FlatList
+                data={menuList}
+                keyExtractor={(item) => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                renderItem={({item, index}) => {
+                    return(
+                        <Text>
+                            {item.name}
+                        </Text>
+                    )
+                }}
+            />
         </View>
     )
 }
