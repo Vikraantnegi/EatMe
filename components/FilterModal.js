4ff4929da7498592fb59488725fbd069fd399afe
vikrant <vikrant.negi74@gmail.com>
2022-01-24 19:54:54 +0530

    Filter Modal BoilerPlate

--- components/FilterModal.js
+++ components/FilterModal.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Modal } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { COLORS } from '../constants';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-const FilterModal = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+const FilterModal = (props) => {
+    const { isVisible, onClose } = {...props}
+    const [showFilterModal, setFilterModal] = useState(isVisible)
+
+    useEffect(() => {
+        if(!showFilterModal){
+            onClose();
+        }
+    }, [showFilterModal])
+    
+    return (
+        <Modal
+            animationType='fade'
+            transparent={true}
+            visible={isVisible}
+        >
+            <View
+                style={{ flex: 1, backgroundColor: COLORS.transparentBlack7 }}
+            >
+                <TouchableWithoutFeedback
+                    onPress={() => setFilterModal(false)}
+                >
+
+                </TouchableWithoutFeedback>
+            </View>
+        </Modal>
+    );
 };
 
 export default FilterModal;
