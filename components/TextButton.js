9f637d5dcb197fcd0820298aea0b7e93dea17cc8
vikrant <vikrant.negi74@gmail.com>
2022-01-25 21:44:33 +0530

    Text Button Update

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,9 +1,9 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import { COLORS, FONTS } from '../constants';
 
 const TextButton = (props) => {
-    const {label, labelStyle, buttonContainerStyle, onPress} = {...props}
+    const {label, labelStyle, buttonContainerStyle, onPress, icon, iconStyle} = {...props}
     return (
         <TouchableOpacity
             style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: COLORS.primary, ...buttonContainerStyle }}
@@ -14,6 +14,12 @@ const TextButton = (props) => {
             >
                 {label}
             </Text>
+            {icon &&
+                <Image
+                    source={icon}
+                    style={{ marginLeft: 5, width: 20, height: 20, ...iconStyle }}
+                />
+            }
         </TouchableOpacity>
     );
 };
