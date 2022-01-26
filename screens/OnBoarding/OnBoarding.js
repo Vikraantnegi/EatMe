5bf08db664fc580baa0aa11ab830dee28fec8ac1
vikrant <vikrant.negi74@gmail.com>
2022-01-26 21:28:18 +0530

    Onboarding List

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -15,25 +15,38 @@ const OnBoarding = () => {
                     resizeMode='contain'
                     style={{ width: SIZES.width*0.5, height: 100 }}
                 />
-
-                <Animated.FlatList
-                    horizontal
-                    pagingEnabled
-                    data={constants.onboarding_screens}
-                    scrollEventThrottle={16}
-                    snapToAlignment="center"
-                    showsHorizontalScrollIndicator={false}
-                    keyExtractor={(item) => `${item.id}`}
-                    renderItem={({item, index}) => {
-                        return(
-                            <View
-                            
-                            >
-                            </View>
-                        )
-                    }}
-                />
             </View>
+            <Animated.FlatList
+                horizontal
+                pagingEnabled
+                data={constants.onboarding_screens}
+                scrollEventThrottle={16}
+                snapToAlignment="center"
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item) => `${item.id}`}
+                renderItem={({item, index}) => {
+                    return(
+                        <View
+                            style={{ width: SIZES.width }}
+                        >
+                            <View
+                                style={{ flex: 3 }}
+                            >
+                                <ImageBackground
+                                    source={item.backgroundImage}
+                                    style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', width: "100%", height: "100%" }}
+                                >
+                                    <Image
+                                        source={item.bannerImage}
+                                        resizeMode='contain'
+                                        style={{ width: SIZES.width * 0.8, height: SIZES.width*0.8, marginBottom: -SIZES.padding }}
+                                    />
+                                </ImageBackground>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
             
         </View>
     )
