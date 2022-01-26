58f6e29cb37f5bc7d83bb0628af6ace295cab368
vikrant <vikrant.negi74@gmail.com>
2022-01-26 21:23:45 +0530

    Onboarding Header

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, Image, Animated } from 'react-native';
+import { constants, images, FONTS, SIZES, COLORS } from '../../constants'
 
 const OnBoarding = () => {
     return (
         <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
+            style={{ flex: 1 }}
         >
-            <Text style={{color: 'black'}}>OnBoarding</Text>
+            <View
+                style={{ position: 'absolute', top: SIZES.height > 800 ? 50 : 25, left: 0, right: 0, alignItems: 'center', justifyContent: 'center' }}
+            >
+                <Image
+                    source={images.logo_02}
+                    resizeMode='contain'
+                    style={{ width: SIZES.width*0.5, height: 100 }}
+                />
+
+                <Animated.FlatList
+                    horizontal
+                    pagingEnabled
+                    data={constants.onboarding_screens}
+                    scrollEventThrottle={16}
+                    snapToAlignment="center"
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={(item) => `${item.id}`}
+                    renderItem={({item, index}) => {
+                        return(
+                            <View
+                            
+                            >
+                            </View>
+                        )
+                    }}
+                />
+            </View>
+            
         </View>
     )
 }
