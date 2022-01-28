77e2bb5ad98479ab0805e759d766890a5b1e7ee4
vikrant <vikrant.negi74@gmail.com>
2022-01-28 20:30:01 +0530

    Input Fields Head

--- components/Auth/FormInput.js
+++ components/Auth/FormInput.js
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
+import { FONTS, SIZES, COLORS } from '../../constants';
 
-const FormInput = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+const FormInput = (props) => {
+  const { containerStyle, label, errorMsg="" } = {...props}
+
+    return (
+        <View style={{ ...containerStyle }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text style={{ color: COLORS.gray, ...FONTS.body4 }}>{label}</Text>
+                {errorMsg && <Text style={{ color: COLORS.red, ...FONTS.body4 }}>{errorMsg}</Text>}
+            </View>
+        </View>
+    );
 };
 
 export default FormInput;
