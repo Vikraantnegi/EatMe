d0df065571d398adab1429204defeecb14ddd7b8
vikrant <vikrant.negi74@gmail.com>
2022-01-30 13:08:10 +0530

    OTP Page Layout

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const App = () => {
                     screenOptions={{
                         headerShown: false
                     }}
-                    initialRouteName={'OnBoarding'}
+                    initialRouteName={'Otp'}
                 >
                     <Stack.Screen
                         name="OnBoarding"
--- screens/Authentication/Otp.js
+++ screens/Authentication/Otp.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import AuthLayout from '../../components/Auth/AuthLayout';
+import { COLORS, FONTS, SIZES } from '../../constants';
+import TextButton from '../../components/Home/TextButton';
+import OTPInputView from '@twotalltotems/react-native-otp-input';
 
-const Otp = () => {
+const Otp = (props) => {
+
+    const [timer, setTimer] = useState(120)
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setTimer(prev => {
+                if(prev > 0){
+                    return prev - 1;
+                } else{
+                    return prev;
+                }
+            })
+        }, 2000)
+        return () => clearInterval(interval)
+    }, [])
+
     return (
-        <View>
-            <Text style={{color: 'black'}}>Otp</Text>
-        </View>
+        <AuthLayout
+            title="OTP Authentication"
+            subTitle="An authentication code has been sent!"
+            titleContainerStyle={{ marginTop: SIZES.padding*2 }}
+        >
+            <View
+                style={{ flex: 1, marginTop: SIZES.padding*2 }}
+            >
+                <OTPInputView
+                    pinCount={4}
+                    style={{ width: '100%', height: 50 }}
+                    codeInputFieldStyle={{ width: 65, height: 65, borderRadius: SIZES.radius, backgroundColor: COLORS.lightGray2, color: COLORS.black, ...FONTS.h3 }}
+                    onCodeFilled={(code) => {
+                        console.log(code)
+                    }}
+                />
+
+                <View
+                    style={{ flexDirection: 'row', justifyContent: 'center', marginTop: SIZES.padding }}
+                >
+                    <Text style={{ color: COLORS.darkGray, ...FONTS.body3 }} >Didn't receive the code?</Text>
+                    <TextButton
+                        label={`Resend (${timer}s)`}
+                        disabled={!(timer === 0)}
+                        buttonContainerStyle={{ marginLeft: SIZES.base, backgroundColor: null }}
+                        labelStyle={{ color: COLORS.primary, ...FONTS.h3 }}
+                        onPress={() => setTimer(120)}
+                    />
+                </View>
+            </View>
+        </AuthLayout>
     )
 }
 
